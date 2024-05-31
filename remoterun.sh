a4e756a75c86a799cf8f8e8eceb4c9aea0d7ac0a
Joas329 <joaquinphilco@hotmail.com>
2024-05-31 10:55:55 -0400

    remoterun: fix generic user path issue.

--- remoterun.sh
+++ remoterun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ssh-copy-id $1@$2
-rsync -avz --progress * $1@$2:"/home/furios/furios-terminal/"
+rsync -avz --progress * $1@$2:"/home/$1/furios-terminal/"
 ssh $1@$2 "pkill -f furios-terminal"
-ssh $1@$2 "cd /home/furios/furios-terminal; meson compile -C build; sudo -S ./build/furios-terminal;"
+ssh $1@$2 "cd /home/$1/furios-terminal; meson compile -C build; sudo -S ./build/furios-terminal;"
